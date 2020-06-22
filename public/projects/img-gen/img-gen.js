827601805a29c76c98a29110f9bceab535e4b451
Zac Smith <zrs3141592@gmail.com>
2020-06-21 22:13:51 -0600

    fix package.json

---
+++ public/projects/img-gen/img-gen.js
@@ -0,0 +1,8 @@
+let topTextInput, bottomTextInput, imgInput, generateBtn, canvas, cyx
+
+function generateImg() {}
+
+function init() {
+  topTextInput = document.getElementById('top-text')
+  bottomTextInput = document.getElementById('bottom-text')
+}
