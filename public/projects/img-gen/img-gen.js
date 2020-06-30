c05fddf61c889f941a9c9d91ef82abee3bae5405
Zac Smith <zrs3141592@gmail.com>
2020-06-29 18:37:21 -0600

    remove styling, breaks with container style

--- public/projects/img-gen/img-gen.js
+++ public/projects/img-gen/img-gen.js
@@ -1,10 +1,18 @@
 let topTextInput, bottomTextInput, imgInput, generateBtn, canvas, ctx
 
-const generateImage = (img) => {
+const generateImage = (img, topText, bottomText) => {
   canvas.width = img.width
   canvas.height = img.height
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   ctx.drawImage(img, 0, 0)
+
+  // let fontsize = canvas.width / 15
+  // ctx.font = fontsize + 'px Impact'
+  // ctx.fillStyle = 'white'
+  // ctx.strokeStyle = 'black'
+  // ctx.lineWidth = fontsize / 15
+
+  // ctx.fillText(topText, canvas.width / 2, 30, canvas.width)
 }
 
 const init = () => {
@@ -19,9 +27,9 @@ const init = () => {
   generateBtn.addEventListener('click', () => {
     let reader = new FileReader()
     reader.onload = function () {
-      let img = new Image()
+      let img = new Image
       img.src = reader.result
-      generateImage(img)
+      generateImage(img, topTextInput.value, bottomTextInput.value)
     }
     reader.readAsDataURL(imgInput.files[0])
   })
