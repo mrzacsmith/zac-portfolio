ae95b91231d080e4dd917685e48b1b7c87ef5d55
Zac Smith <zrs3141592@gmail.com>
2020-06-29 21:51:58 -0600

    create generateBtn ~ not working

--- public/projects/img-gen/img-gen.js
+++ public/projects/img-gen/img-gen.js
@@ -1,8 +1,30 @@
-let topTextInput, bottomTextInput, imgInput, generateBtn, canvas, cyx
+let topTextInput, bottomTextInput, imgInput, generateBtn, canvas, ctx
 
-function generateImg() {}
+function generateImage(img) {
+  canvas.width = img.width
+  canvas.height = img.height
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(img, 0, 0)
+}
 
 function init() {
   topTextInput = document.getElementById('top-text')
   bottomTextInput = document.getElementById('bottom-text')
+  imgInput = document.getElementById('img-input')
+  generateBtn = document.getElementById('generate-btn')
+  canvas = document.getElementById('img-canvas')
+  ctx = canvas.getContext('2d')
+  canvas.width = canvas.height = 0
+
+  generateBtn.addEventListener('click', () => {
+    let reader = new FileReader()
+    reader.onload = function () {
+      let img = new Image()
+      img.src = reader.result
+      generateImage(img)
+    }
+    reader.readAsDataURL(imgInput.files[0])
+  })
 }
+
+init()
