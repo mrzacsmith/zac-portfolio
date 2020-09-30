0239c6d6b1c7107081d81e4341023f1638927529
Zac Smith <zrs3141592@gmail.com>
2020-09-29 21:34:41 -0600

    comment out

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from 'react';
+import React from 'react'
 import { Route } from 'react-router-dom'
-import './App.css';
+import './App.css'
 import AboutMe from './components/AboutMe'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import LandingPage from './components/LandingPage'
 import Blog from './components/Blog'
 
-
 function App() {
   return (
     <div className="App">
-
-        <div>
-        <Route to='/' component={LandingPage} />
-        <Route to='/about' component={AboutMe} />
-        <Route to='/projects' component={Projects} />
-        <Route to='/blog' component={Blog} />
-        <Route to='/contact' component={Contact} />
-        </div>
-
+      <div>
+        <Route to="/" component={LandingPage} />
+        {/* <Route to='/about' component={AboutMe} />
+        <Route to='/projects' component={Projects} /> */}
+        <Route to="/blog" component={Blog} />
+        <Route to="/contact" component={Contact} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
